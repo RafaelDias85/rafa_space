63878152c2a52c8a157c97edcd7fec1450457bf4
Bowie <rafaeldiassilv@gmail.com>
2024-01-29 23:48:29 -0300

    Alterações no Django Admin

--- galeria/models.py
+++ galeria/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+class Fotografia (models.Model):
+
+    OPCOES_CATEGORIA = [
+        ("NEBULOSA", "Nebulosa"),
+        ("ESTRELA", "Estrela"),
+        ("GALÁXIA", "Galáxia"),
+        ("PLANETA", "Planeta"),
+    ]
+
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    categoria = models.CharField(max_length=100, choices=OPCOES_CATEGORIA, default='')
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    publicada = models.BooleanField(default=False)
+    data_fotografia = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self):
+        return f"Fotografia [nome={self.nome}]"
