575f9f90d79152194a622567952341820a03913a
Bowie <rafaeldiassilv@gmail.com>
2024-02-18 09:31:40 -0300

    Finalizando Estudo de django ps. ainda tem mais rsrsrs

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from galeria.views import *
+from .views import *
 
 urlpatterns = [
     path ('', index, name='home'),
--- galeria/views.py
+++ galeria/views.py
@@ -11,5 +11,12 @@ def imagem(request, foto_id):
     return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
 def buscar(request):
-
-    return render(request, "galeria/buscar.html")
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar'] #Referencia ao name='buscar'
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar) #O argumento nome__icontains irá buscar se, dentro do nome
+                                                                            #que estamos conferindo de um objeto em específico, existe 
+                                                                            #alguma parte que faz sentido com o nome que está sendo buscado. 
+                                                                            # Ele irá buscar se pelo menos metade do nome faz sentido em relação ao que estamos buscando.
+    return render(request, "galeria/buscar.html", {"cards": fotografias})
